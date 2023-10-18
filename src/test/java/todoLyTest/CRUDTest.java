9ca5d008a8bfa1a69aaa2930dc864a1368e3896e
Samuel Escobar <samueb8@gmail.com>
2023-10-18 18:30:52 -0400

    commit 2

--- src/test/java/todoLyTest/CRUDTest.java
+++ src/test/java/todoLyTest/CRUDTest.java
@@ -3,10 +3,6 @@ package todoLyTest;
 import config.Configuration;
 import factoryRequest.FactoryRequest;
 import factoryRequest.RequestInfo;
-import models.Item;
-import models.Project;
-import org.json.JSONArray;
-import org.json.JSONException;
 import org.json.JSONObject;
 import org.junit.jupiter.api.Test;
 
@@ -25,79 +21,39 @@ public class CRUDTest extends TestBaseAuthBasic {
         createUser();
         Configuration.user = email;
         Configuration.password = password;
+        requestInfo = new RequestInfo();
+        String credential= Configuration.user+":"+Configuration.password;
+        requestInfo.setHeaders("Authorization","Basic "+ Base64.getEncoder().encodeToString(credential.getBytes()).toString());
         JSONObject body = new JSONObject();
         body.put("Content","Proyecto1Pregunta1");
         this.createProject(Configuration.host + "/api/projects.json", body, post);
         this.deleteUser();
         body = new JSONObject();
-        body.put("Content","null");
-        this.createProject(Configuration.host + "api/projects.json", body, post);
+        body.put("ErrorCode", 105);
+        this.createProjectFail(Configuration.host + "/api/projects.json", body, post);
     }
 
     @Test
-    public void createUpdateDeleteProject(){
+    public void preguntaDos(){
+        List<Integer> ids = new ArrayList<Integer>();
+        List<JSONObject> contents = new ArrayList<JSONObject>();
         JSONObject body = new JSONObject();
-        body.put("Content","Refactor");
-
-        this.createProject(Configuration.host + "/api/projects.json", body, post);
-        int idProject = response.then().extract().path("Id");
-        this.readProject(idProject, get, body);
-        body.put("Content","Refactor1");
-        this.updateProject(Configuration.host + "/api/projects/" + idProject + ".json", body, put);
-        this.deleteProject(idProject, delete, body);
-    }
-
-    @Test
-    public void deleteAllProjects(){
-        requestInfo.setUrl(Configuration.host + "/api/projects.json");
-        response = FactoryRequest.make(get).send(requestInfo);
-
-        String answer = response.getBody().asString();
-
-        List<Project> projects = extractProjects(answer);
-
-        for (Project project : projects) {
-            JSONObject body = new JSONObject();
-            body.put("Content", project.getContent());
-            deleteProject(project.getId(), delete, body);
+        for(int i = 0; i < 4; i++){
+            body = new JSONObject();
+            body.put("Content","Proyecto" + (i+1) + "Pregunta2");
+            contents.add(body);
+            this.createProject(Configuration.host + "/api/projects.json", body, post);
+            ids.add(response.then().extract().path("Id"));
         }
-    }
 
-    public static List<Project> extractProjects(String jsonString) {
-        List<Project> projects = new ArrayList<>();
-        try {
-            JSONArray jsonArray = new JSONArray(jsonString);
-            for (int i = 0; i < jsonArray.length(); i++) {
-                JSONObject projectObject = jsonArray.getJSONObject(i);
-                int id = projectObject.getInt("Id");
-                String content = projectObject.getString("Content");
-                Project project = new Project(id, content);
-                projects.add(project);
-            }
-        } catch (JSONException e) {
-            e.printStackTrace();
+        for(int i = 0; i < ids.size(); i++){
+            this.deleteProject(ids.get(i), delete, contents.get(i));
         }
-        return projects;
     }
 
     private void deleteProject(int idProject, String delete, JSONObject body) {
         requestInfo.setUrl(Configuration.host + "/api/projects/" + idProject + ".json");
         response = FactoryRequest.make(delete).send(requestInfo);
-        response.then().statusCode(200).
-                body("Content", equalTo(body.get("Content")));
-    }
-
-    private void updateProject(String host, JSONObject body, String put) {
-        requestInfo.setUrl(host)
-                .setBody(body.toString());
-        response = FactoryRequest.make(put).send(requestInfo);
-        response.then().statusCode(200).
-                body("Content", equalTo(body.get("Content")));
-    }
-
-    private void readProject(int idProject, String get, JSONObject body) {
-        requestInfo.setUrl(Configuration.host + "/api/projects/" + idProject + ".json");
-        response = FactoryRequest.make(get).send(requestInfo);
         response.then().statusCode(200).
                 body("Content", equalTo(body.get("Content")));
     }
@@ -108,6 +64,13 @@ public class CRUDTest extends TestBaseAuthBasic {
         response = FactoryRequest.make(post).send(requestInfo);
         response.then().statusCode(200).
                 body("Content", equalTo(body.get("Content")));
+    }
+    private void createProjectFail(String host, JSONObject body, String post) {
+        requestInfo.setUrl(host)
+                .setBody(body.toString());
+        response = FactoryRequest.make(post).send(requestInfo);
+        response.then().statusCode(200).
+                body("ErrorCode", equalTo(body.get("ErrorCode")));
     }
 
 }
